17ab9eddcc666510d4c8e6d6a9e2ed0454de22a5
abnliraq <supergamez.sg@gmail.com>
2020-11-10 01:39:06 +0300

    Update frybot.py

--- userbot/plugins/frybot.py
+++ userbot/plugins/frybot.py
@@ -5,23 +5,23 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot.utils import admin_cmd
 
-@borg.on(admin_cmd("frybot ?(.*)"))
+@borg.on(admin_cmd("تشويه ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("```Reply to any user message.```")
+       await event.edit("```رد على اي مستخدم.```")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.media:
-       await event.edit("```reply to text message```")
+       await event.edit("```رد على رسالة```")
        return
     chat = "@image_deepfrybot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("```Reply to actual users message.```")
+       await event.edit("```لا ترد على بوت.```")
        return
-    await event.edit("```Processing```")
+    await event.edit("```جارر....@X3RAQE```")
     async with borg.conversation(chat) as conv:
           try:     
               response = conv.wait_event(events.NewMessage(incoming=True,from_users=432858024))
