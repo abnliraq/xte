cf1e6146126132bb23ea7dc1d8bcb58dc8049d3c
abnliraq <supergamez.sg@gmail.com>
2020-11-10 03:33:02 +0300

    Add files via upload

---
+++ userbot/plugins/Zagrafa.py
@@ -0,0 +1,41 @@
+#© klanr aloosh
+#  To @aaaDa
+
+"""Corona: First Send Your Country name then reply .corona
+"""
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern="zag ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to any user message.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```Reply to text message```")
+       return
+    chat = '@X3raqebot'
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    await event.edit("```wite zagrph you...```")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1143836767))
+              await event.client.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock me (@X3raqebot) u Nigga```")
+              return
+          if response.text.startswith("Hi!"):
+             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
+          else: 
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
